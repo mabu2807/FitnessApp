661b663278aefbd27320b969e81f00a06b52b64e
StabilesMuli <justin.schwab@t-online.de>
2023-05-26 21:27:10 +0200

    social media in footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -139,6 +139,11 @@
 <footer class="footer">
   <div class="footer-content">
     <p class="footer-text">&copy; 2023 Fitness Tracker. All rights reserved.</p>
+    <a class="linkFooter" href="https://www.youtube.com/@RoswitaRuhl"><i class="fa-brands fa-youtube fa-2x"></i></a>
+    <a class="linkFooter" href="https://www.instagram.com/ksvlangen/"><i class="fa-brands fa-instagram fa-2x"></i></a>
+    <a class="linkFooter" href="https://twitter.com/mibi61?s=21&t=O8Ege5KSxyYHHBAtUfSaJQ"><i class="fa-brands fa-square-twitter fa-2x"></i></a>
+    <a class="linkFooter" href="https://www.linkedin.com/in/luca-chmielarski?original_referer="><i class="fa-brands fa-linkedin fa-2x"></i></a>
+
   </div>
 </footer>
 
@@ -409,15 +414,25 @@ button[type="submit"]:hover {
 }
 
 .footer-content {
+  margin-right: 30%;
+  margin-left: 30%;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   height: 100%;
 }
 
 .footer-text {
   font-size: 14px;
   margin: 0;
+}
+
+i:hover {
+  color:#888;
+}
+
+.linkFooter {
+  color: white;
 }
 
 
