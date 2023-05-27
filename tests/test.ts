c020283f05db83ce7cd2819df05b82656935c1c3
Schokobonlean <s212529@student.dhbw-mannheim.de>
2023-05-27 15:22:50 +0200

    added some tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,23 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+
+test('Index Page: has expected Title', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page).toHaveTitle("Fitness Tracker");
+});
+
+test('Index Page: GetStarted button is visible', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByRole('link', { name: 'Get Started'})).toBeVisible();
+});
+
+test('Index Page: GetStarted button works', async ({ page }) => {
+	await page.goto('/');
+	await page.getByRole('link', { name: 'Get Started'}).click();
+	await expect(page).toHaveURL("/getStarted");
+});
+
+test('Index Page: course section is visible', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByRole('heading', { name: 'Unsere Kurse'})).toBeVisible();
 });
