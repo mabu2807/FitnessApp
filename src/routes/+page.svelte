9f72ccfd61268ca05091d6f401b4385c250ba4bc
Marcel Bulling <marcel_bulling@gmx.de>
2023-05-29 14:43:33 +0200

    init prisma

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script>
+    let users;
+
+    async function showUsers() {
+        const response = await fetch('/');
+        users = await response.json();
+    }
+    users = showUsers()
+    console.log(users);
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
