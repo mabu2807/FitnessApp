5881b925cdaa4a1b9788be82388fe97e33f521c3
Adrian Hagen <adrian.hagen@sap.com>
2023-05-31 14:31:37 +0200

    Added Create Page for user

--- src/routes/user/create/+page.server.ts
+++ src/routes/user/create/+page.server.ts
@@ -3,36 +3,43 @@ import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
 export const actions = {
-    // 1.
     default: async ({ request }) => {
         const data = await request.formData();
 
         const email = data.get("email")
         const username = data.get("username")
-        const userDetailsId = data.get("userDetailsId")
-        const foodDiaryId = data.get("foodDiaryId")
+        const password = data.get("password")
 
-        // 2.
-        if (!email || !username || !userDetailsId || !foodDiaryId) {
-            return fail(400, { email, username, missing: true });
+        if (!email || !username || !password) {
+            return fail(400, { email, username, password, missing: true });
         }
 
-        // 3.
-        if (typeof email != "string" || typeof username != "string" || typeof userDetailsId != "number" || typeof foodDiaryId != "number") {
+        if (typeof email != "string" || typeof username != "string" || typeof password != "string") {
             return fail(400, { incorrect: true })
         }
 
-        // 4.
+        //Validate email
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            return fail(422, {email})
+        }
+
+        //Check if email already exists
+        const user = await prisma.user.findUnique({
+            where: { email: email }
+        });
+        if(user){
+            return fail(409, {email})
+        }
+
         await prisma.user.create({
             data: {
-                email,
-                username,
-                userDetails: { connect: { userId: userDetailsId } },
-                foodDiary: { connect: { id: foodDiaryId } }
+                email: email,
+                username: username,
+                password: password
             },
         });
 
-        //5.
         throw redirect(303, `/`)
     }
 } satisfies Actions;
