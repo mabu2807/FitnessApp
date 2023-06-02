5f06dad1d726da2428da0af88fbc74a7718890eb
StabilesMuli <justin.schwab@t-online.de>
2023-06-02 21:46:18 +0200

    footer design + responsive

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,18 +1,20 @@
 <footer class="footer">
 	<div class="footer-content">
-		<p class="footer-text">&copy; 2023 Fitness Tracker. All rights reserved.</p>
-		<a class="linkFooter" href="https://www.youtube.com/@RoswitaRuhl"
-			><i class="fa-brands fa-youtube fa-2x" /></a
-		>
-		<a class="linkFooter" href="https://www.instagram.com/ksvlangen/"
-			><i class="fa-brands fa-instagram fa-2x" /></a
-		>
-		<a class="linkFooter" href="https://twitter.com/mibi61?s=21&t=O8Ege5KSxyYHHBAtUfSaJQ"
-			><i class="fa-brands fa-square-twitter fa-2x" /></a
-		>
-		<a class="linkFooter" href="https://www.linkedin.com/in/luca-chmielarski?original_referer="
-			><i class="fa-brands fa-linkedin fa-2x" /></a
-		>
+		<p class="footer-text">&copy; 2023 FitnessHub. All rights reserved.</p>
+		<div class="footer-icons">
+			<a class="linkFooter" href="https://www.youtube.com/@RoswitaRuhl"
+				><i class="fa-brands fa-youtube" /></a
+			>
+			<a class="linkFooter" href="https://www.instagram.com/ksvlangen/"
+				><i class="fa-brands fa-instagram" /></a
+			>
+			<a class="linkFooter" href="https://twitter.com/mibi61?s=21&t=O8Ege5KSxyYHHBAtUfSaJQ"
+				><i class="fa-brands fa-square-twitter" /></a
+			>
+			<a class="linkFooter" href="https://www.linkedin.com/in/luca-chmielarski?original_referer="
+				><i class="fa-brands fa-linkedin" /></a
+			>
+		</div>
 	</div>
 </footer>
 
@@ -21,25 +23,48 @@
 		background-color: #393e46;
 		color: #ffffff;
 		width: 100%;
-		height: 60px;
-		padding: 22px 0;
+		height: 5.5vh;
+		padding: 1.8vw 0;
 	}
 	.footer-content {
-		margin-right: 30%;
-		margin-left: 30%;
 		display: flex;
 		align-items: center;
 		justify-content: space-between;
 		height: 100%;
 	}
+	.footer-icons {
+		left: 80%;
+		width: 20%;
+		font-size: 2.4vw;
+	}
 	.footer-text {
-		font-size: 14px;
-		margin: 0;
+		margin-left: 40%;
+
+		font-size: 1.4vw;
 	}
 	.footer i:hover {
 		color: #888;
 	}
 	.linkFooter {
+		margin-right: 1.5vw;
 		color: white;
+	}
+	@media (max-width: 1100px) {
+		.footer-text {
+			font-size: 1.7vw;
+		}
+		.footer-icons {
+			font-size: 2.7vw;
+		}
+	}
+
+	@media (max-width: 700px) {
+		.footer-text {
+			font-size: 2.2vw;
+			margin-left: 30%;
+		}
+		.footer-icons {
+			font-size: 3vw;
+		}
 	}
 </style>
