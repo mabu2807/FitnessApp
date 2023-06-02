f89a7a84b0fc598dbd7a76ee85637286a91395f9
Koordis <AleksGeorg@outlook.de>
2023-06-02 16:09:00 +0200

    finished navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,123 @@
 <script>
+	import { onMount } from "svelte/internal";
+
 	let isMenuOpen = false;
+	let isProfileMenuOpen = false;
+	$: loggedIn = true;
+	// @ts-ignore
+	let clickOutsideListener;
+
+	let settings = [
+		{
+			setting: "Persönliche Daten",
+			icon: "fa-solid fa-user"
+		},
+		{
+			setting: "Passwort ändern",
+			icon: "fa-solid fa-key"
+		}, 
+		{
+			setting: "Einstellungen",
+			icon: "fa-solid fa-gear"
+		}
+	]
+
+	let testUser = {
+		name: "Georgiev, Aleks",
+		
+	}
 
 	function toggleMenu() {
 		isMenuOpen = !isMenuOpen;
 	}
+
+	function toggleProfileMenu() {
+		isProfileMenuOpen = !isProfileMenuOpen;
+	}
+
+	onMount(() => {
+    // @ts-ignore
+    clickOutsideListener = (event) => {
+      const sidebar = document.querySelector('.sidebar');
+      const profileWrapper = document.querySelector('.profile-wrapper');
+      const targetElement = event.target;
+
+      if (
+        profileWrapper &&
+        !profileWrapper.contains(targetElement) &&
+        // @ts-ignore
+        !sidebar.contains(targetElement) &&
+        !targetElement.classList.contains('hamburger-lines')
+      ) {
+        isProfileMenuOpen = false;
+      }
+    };
+
+    document.addEventListener('click', clickOutsideListener);
+
+    return () => {
+      // @ts-ignore
+      document.removeEventListener('click', clickOutsideListener);
+    };
+  });
+
 </script>
 
 <nav class="navbar">
 	<div class="navbar-container" class:open={isMenuOpen}>
 		<h1 class="logo">FitnessHub</h1>
-		<!-- Logo auf der linken Seite -->
+		
+		{#if loggedIn}
 		<ul class="menu-items">
-			<li><a href="/">Home</a></li>
-			<li><a href="/kategorie">Workouts</a></li>
-			<li><a href="/progress">Progress</a></li>
-			<button class="login-button">Login</button>
+			<li><a class="a" href="/">Home</a></li>
+			<li><a class="a" href="/kategorie">Workouts</a></li>
+			<li><a class="a" href="/progress">Progress</a></li>
+			<li><a class="a" href="/">Ernährungsplan</a></li>
 		</ul>
+
+		<!-- <Avatar src="/src/assets/profile.jpg"  actionParams="#Apollo" /> -->
+		<div class="profile-wrapper" on:click={toggleProfileMenu}>
+			 <a><img src="/src/assets/profile.jpg" alt=""> </a>
+		</div>
+		{#if isProfileMenuOpen}
+		<div class="sidebar">
+			<div class="profile-title-container">
+
+				<div class="profile-wrapper-sidebar" on:click={toggleProfileMenu}>
+					<a><img src="/src/assets/profile.jpg" alt=""> </a>
+				</div>
+				<p class="user-title">{testUser.name}</p>
+			</div>
+			<span class="bar"></span>
+			<div class="settings-wrapper">
+				{#each settings as setting}
+				
+					<a href="/" class="setting">
+						<div class="setting-icon">
+							<i class="{setting.icon}"></i>
+							
+						</div>
+						<p>{setting.setting}</p>
+					</a>
+				
+				{/each}
+			</div>
+			<div style="flex-grow: 1;"></div>
+			<div class="exit-wrapper">
+				<div class="close-icon">
+					<i class="fa-sharp fa-solid fa-angles-right" on:click={toggleProfileMenu}></i>
+				</div>
+				<div class="logOut">
+					<button class="logOut-button">Abmelden <i class="fa-solid fa-right-from-bracket" style="margin-left: 10px;"></i></button>
+				</div>
+			</div>
+		</div>
+	{/if}
+		
+		{/if}
+		{#if loggedIn == false}
+		<a  on:click={() => login} class="login-button">Login</a>
+		{/if}
 		<div class="hamburger-lines" on:click={toggleMenu}>
 			<span class="line line1" />
 			<span class="line line2" />
@@ -35,15 +137,16 @@
 		margin: 0;
 	}
 
+	
 	.navbar {
 		background: #393e46;
 		color: white;
-
+		box-shadow: 0 0 10px black;
 		z-index: 100;
 		position: relative;
 		padding-left: 20px; /* Innenabstand auf der linken Seite */
 	}
-
+	
 	.navbar-container {
 		display: flex;
 		justify-content: space-between;
@@ -52,26 +155,200 @@
 		padding-left: 80px;
 	}
 
+	.sidebar {
+		display: flex;
+		flex-direction: column;
+		position: fixed;
+		text-align: center;
+		top: 0;
+		right: 0;
+		width: 300px; 
+		height: 100vh;
+		background-color: #393e46;
+		box-shadow: -10px 0px 10px 1px rgba(0, 0, 0, 0.244);
+		transition: 0.3s ease all;
+	}
+
+	.profile-title-container {
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+		height: 15%;
+		
+	}
+
+	.settings-wrapper {
+		display: flex;
+		flex-direction: column;
+		
+		width: 100%;
+		height: auto;
+		margin-top: 7%;
+		
+	}
+
+	.setting {
+		display: flex;
+		flex-direction: row;
+		margin: 20px;
+		margin-left: 50px;
+		text-decoration: none;
+		color: white;
+		
+	}
+
+	.close-icon {
+		
+		width: 49%;
+    height: 26%;
+    font-size: 36px;
+		color: white;
+		margin: 6px 0 40px 0;
+	}
+
+	.close-icon i {
+		cursor: pointer;
+		
+		position: relative;
+		animation: indicator 1.5s ease-in-out  alternate;
+		animation-iteration-count: 2;
+	}
+
+	.close-icon i:hover {
+		color: #7eadff;
+	}
+
+	@keyframes indicator {
+		0% {
+    left: 0;
+  }
+  50% {
+    left: 10%;
+  }
+  75% {
+	left: 5%;
+  }
+  100% {
+    left: 0;    
+  }
+
+	}
+
+	.logOut {
+		padding-right: 22px;
+	}
+
+	.logOut-button {
+		cursor: pointer;
+		
+		padding: 14px 16px;
+		border: 3px solid white;
+		color: white;
+		border-radius: 30px;
+		background-color: #393e46;
+		font-weight: 700;
+		
+	}
+
+	.logOut-button:hover {
+		box-shadow: 0 0 10px white;
+		color: #393e46;
+		background-color: white;
+		transition: .4s ease all;
+	}
+	.exit-wrapper {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		width: 100%;
+		height: 9%;
+		color: white;
+		margin-bottom: 20px;
+		margin-right: 50px;
+	}
+	.setting-icon {
+		margin-right: 20px;
+	}
+
+	.user-title {
+		margin-top: 10%;
+	}
+
+	.bar {
+		margin-left: 20%;
+		border-radius: 200px;
+		width: 60%;
+		height: 4px;
+		background-color: white;
+	}
+
+	.profile-wrapper-sidebar {
+		width: 23%;
+    	height: 62%;
+		cursor: pointer;
+		
+    	margin: 20px 30px;
+		border: 2px solid white;
+		border-radius: 400px;
+		box-shadow: 0 0 10px white;
+	}
+
+	.profile-wrapper-sidebar img {
+		height: 101%;
+    width: 100%;
+    object-fit: cover;
+    border-radius: 400px;
+	}
+
+	.profile-wrapper {
+		cursor: pointer;
+		height: 84%;
+		width: 5%;
+		margin-right: 60px;
+		border: 2px solid white;
+		border-radius: 400px;
+		box-shadow: 0 0 10px white;
+	}
+
+	.profile-wrapper img {
+		height: 101%;
+    width: 100%;
+    object-fit: cover;
+    border-radius: 400px;
+	}
+
 	.menu-items {
 		display: flex;
-		list-style: none;
-		margin-right: 20px; /* Innenabstand auf der rechten Seite der Reiter */
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		margin: 0 20px;
 	}
 
 	.menu-items li {
-		margin-left: 1.5rem;
 		font-size: 1.3rem;
+		list-style: none;
 	}
-
-	.navbar a {
+	
+	.navbar .a {
+		margin: .5rem 1.5rem;
 		color: white;
 		text-decoration: none;
-		font-weight: 500;
+		font-weight: 600;
 		transition: color 0.3s ease-in-out;
 	}
 
 	.navbar a:hover {
-		color: #117964;
+		color: #7eadff;
+		font-weight: 700;
+	}
+
+	.profile-menu {
+		display: none;
+		cursor: pointer;
+		position: relative;
+		width: 30px;
+		height: 20px;
 	}
 
 	.hamburger-lines {
@@ -105,10 +382,13 @@
 	}
 
 	.login-button {
-		background-color: #117964;
-		color: #fff;
-		border: none;
-		padding: 8px 16px;
+		background-color:  #377ffb;
+		color: white;
+		font-weight: 700;
+		border: 2px solid #377ffb;
+		margin-right: 80px;
+		padding: 5px 30px;
+		
 		border-radius: 4px;
 		font-size: 1.2rem;
 		cursor: pointer;
@@ -116,7 +396,8 @@
 	}
 
 	.login-button:hover {
-		background-color: #0e604c;
+		color: #d1e2ff;
+		background-color: #393e46;
 	}
 
 	@media (max-width: 768px) {
@@ -129,13 +410,12 @@
 		.menu-items {
 			display: none;
 			position: absolute;
-
 			top: 100%;
 			right: 0;
 			width: 100%;
 			background-color: #fff;
 			padding: 20px;
-			box-shadow: 5px 0px 10px 0px #aaa;
+				
 		}
 
 		.login-button {
@@ -173,9 +453,14 @@
 			box-shadow: 5px 0px 10px 0px #aaa;
 		}
 
+		li {
+			margin: 15px;
+		}
+
 		.navbar-container.open .menu-items {
 			display: flex;
 			flex-direction: column;
+			padding-top: 20px;
 		}
 
 		.hamburger-lines {
