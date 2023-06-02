e191f23c120d2becf734e31ac77cb473c9a8f9c1
Adrian Hagen <adrian.hagen@sap.com>
2023-06-02 08:49:24 +0200

    Improved Code for lint

--- src/lib/emails/registration.svelte
+++ src/lib/emails/registration.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Button, Container, Head, Hr, Html, Img, Preview, Section, Text } from 'svelte-email';
+	import { Container, Head, Html, Preview, Section, Text } from 'svelte-email';
 
 	export let firstName = 'Thomas';
 
@@ -15,40 +15,10 @@
 		padding: '20px 0 48px'
 	};
 
-	const logo = {
-		margin: '0 auto'
-	};
-
 	const paragraph = {
 		fontFamily,
 		fontSize: '16px',
 		lineHeight: '26px'
-	};
-
-	const btnContainer = {
-		textAlign: 'center' as const
-	};
-
-	const button = {
-		fontFamily,
-		backgroundColor: '#5F51E8',
-		borderRadius: '3px',
-		color: '#fff',
-		fontSize: '16px',
-		textDecoration: 'none',
-		textAlign: 'center' as const,
-		display: 'block'
-	};
-
-	const hr = {
-		borderColor: '#cccccc',
-		margin: '20px 0'
-	};
-
-	const footer = {
-		fontFamily,
-		color: '#8898aa',
-		fontSize: '12px'
 	};
 </script>
 
@@ -60,7 +30,7 @@
 			<Text style={paragraph}>Hi {firstName}, welcome to FitnessHub!</Text>
 			<Text style={paragraph}>Please click on the following button to verify your email:</Text>
 			<a href="https://www.google.com">
-				<button>Verify Email</button>    
+				<button>Verify Email</button>
 			</a>
 		</Container>
 	</Section>
--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,44 +1,44 @@
-import prisma from "$lib/prisma";
+import prisma from '$lib/prisma';
 import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from '../login/$types';
 
 const validateEmail = (email: string) => {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
-}
+	return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
 
 export const actions = {
-    default: async ({ request }) => {
-        const data = await request.formData();
+	default: async ({ request }) => {
+		const data = await request.formData();
 
-        const email = data.get("email")
-        const password = data.get("password")
+		const email = data.get('email');
+		const password = data.get('password');
 
-        if (!email || !password) {
-            return fail(400, { email, password, missing: true });
-        }
+		if (!email || !password) {
+			return fail(400, { email, password, missing: true });
+		}
 
-        if (typeof email != "string" || typeof password != "string") {
-            return fail(400, { incorrect: true })
-        }
+		if (typeof email != 'string' || typeof password != 'string') {
+			return fail(400, { incorrect: true });
+		}
 
-        if(!validateEmail){
-            return fail(422, {email})
-        }
+		if (!validateEmail) {
+			return fail(422, { email });
+		}
 
-        //Check if email already exists
-        const user = await prisma.user.findUnique({
-            where: { email: email }
-        });
-        if(!user){
-            return fail(404, {email})
-        }
+		//Check if email already exists
+		const user = await prisma.user.findUnique({
+			where: { email: email }
+		});
+		if (!user) {
+			return fail(404, { email });
+		}
 
-        if(user.password !== password) {
-            return fail(401, {email, password})
-        }
+		if (user.password !== password) {
+			return fail(401, { email, password });
+		}
 
-        console.log("Login Successful!")
+		console.log('Login Successful!');
 
-        throw redirect(303, `/`)
-    }
+		throw redirect(303, `/`);
+	}
 } satisfies Actions;
