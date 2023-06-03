abf1f824bdb0b966a9bd75d024fbe458b189333f
nora <nora.klemp@sap.com>
2023-06-03 11:09:15 +0200

    dialog to add meals

--- src/routes/nutrition/+page.svelte
+++ src/routes/nutrition/+page.svelte
@@ -197,7 +197,7 @@
 		color: black;
 	}
 	.textAddMealTitle {
-		color: red;
+		color: black;
 		align-self: center;
 		justify-content: center;
 		text-align: center;
--- src/routes/nutrition/dialogAdd.svelte
+++ src/routes/nutrition/dialogAdd.svelte
@@ -4,9 +4,29 @@
 	const closeClick = () => {
 		dialogAdd.close();
 	};
+
+	function saveChanges() {
+		//let inputMeal = document.getElementById("meal").value;
+	}
 </script>
 
 <dialog bind:this={dialogAdd} on:close>
 	<p>Neue Mahlzeit hinzufügen</p>
+	<form method="#">
+		<label for="category">Wähle eine Kategorie:</label>
+		<select name="category" id="mealCat">
+			<option value="breakfast">Frühstück</option>
+			<option value="lunch">Mittagessen</option>
+			<option value="dinner">Abendessen</option>
+			<option value="snack">Snack</option>
+		</select>
+		<br />
+		<label for="meal">Gericht</label>
+		<input type="text" id="meal" /><br />
+		<label for="calories">Kalorien</label>
+		<input type="text" id="calories" /><br /><br />
+	</form>
+
 	<button on:click={closeClick}>Close</button>
+	<button type="submit" on:click={saveChanges} on:click={closeClick}>Save</button>
 </dialog>
