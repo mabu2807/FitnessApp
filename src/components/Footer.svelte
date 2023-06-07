1fab33b6b3428b39a84794edbe9fd99d1b022aca
StabilesMuli <justin.schwab@t-online.de>
2023-06-08 00:44:13 +0200

    footer color bug

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,16 +3,16 @@
 	  <p class="text-sm md:text-base">© 2023 FitnessHub. All rights reserved.</p>
 	  <div class="flex ml-10">
 		<a class="mr-2" href="https://www.youtube.com/@RoswitaRuhl">
-			<i class="fab fa-youtube text-black hover:text-gray-500 text-3xl"></i>
+			<i class="fab fa-youtube text-black hover:text-gray-500 text-3xl dark:text-white"></i>
 		</a>
 		<a class="mr-2" href="https://www.instagram.com/ksvlangen/">
-		  <i class="fab fa-instagram text-black hover:text-gray-500 text-3xl"></i>
+		  <i class="fab fa-instagram text-black hover:text-gray-500 text-3xl dark:text-white"></i>
 		</a>
 		<a class="mr-2" href="https://twitter.com/mibi61?s=21&t=O8Ege5KSxyYHHBAtUfSaJQ">
-		  <i class="fab fa-twitter-square text-black hover:text-gray-500 text-3xl"></i>
+		  <i class="fab fa-twitter-square text-black hover:text-gray-500 text-3xl dark:text-white"></i>
 		</a>
 		<a class="mr-2" href="https://www.linkedin.com/in/luca-chmielarski?original_referer=">
-		  <i class="fab fa-linkedin text-black hover:text-gray-500 text-3xl"></i>
+		  <i class="fab fa-linkedin text-black hover:text-gray-500 text-3xl dark:text-white"></i>
 		</a>
 	  </div>
 	</div>
