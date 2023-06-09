3d45af11112dd1a8f3a0bc64be56aadc9b31287c
StabilesMuli <justin.schwab@t-online.de>
2023-06-09 16:23:40 +0200

    getStarted final

--- src/routes/getStarted/+page.svelte
+++ src/routes/getStarted/+page.svelte
@@ -1,30 +1,38 @@
-<body>
-	<div class="first-container">
-		<div class="first-wrapper">
-			<div class="first-title-wrapper">
-				<h2 class="first-title">Willkommen</h2>
-				<img src="Logo.jpg" alt="logo" />
+
+	<section class="flex items-center justify-center">
+		<!-- class first-wrapper -->
+		<div class="card red-gradient lg:w-2/3 xl:w-1/2 w-3/4 m-10 py-10 sm:px-12 px-8 shadow-xl">
+			<div class="text-center mb-8">
+				<h2 class="md:h2 h1">Willkommen bei FitnessHub!</h2>
+				<!-- <img src="Logo.jpg" alt="logo" /> -->
 			</div>
 
-			<div class="first-description-wrapper">
-				<p>
+		
+				<p class="mb-16">
 					Hier kannst du deine Ziele festlegen und deine Fortschritte verfolgen. Bist du bereits
 					registriert? Klicke auf den Button unten, um auf dein Konto zuzugreifen. Wenn nicht, melde
 					dich jetzt an und starte deine Fitnessreise mit uns. Zusammen erreichen wir deine Ziele!
 				</p>
-				<p>Du hast bereits ein Account?</p>
-			</div>
+				<p class="mb-2 sm:ml-0 ml-2 w-1/2 text-tertiary-500 dark:text-primary-500">Du hast bereits einen Account?</p>
+	
 
-			<div class="first-button-">
-				<a href="/login">Melde dich an!</a>
-				<a href="/getStarted/goals" class="first-next-button">Registrierung</a>
+
+			<div class="flex justify-between">
+				<a href="/login"><span class="btn variant-filled md:px-7 px-5 py-2 transition duration-400 hover:bg-tertiary-500 dark:hover:bg-primary-500">Melde dich an!</span></a>
+				<a href="/getStarted/goals"><span class="btn variant-filled md:px-7 px-5 py-2 transition duration-400 hover:bg-tertiary-500 dark:hover:bg-primary-500">Registrierung</span></a>
 			</div>
-		</div>
-	</div>
-</body>
+		</section>
+
 
 <style>
-	.first-container {
+
+.red-gradient {
+		background-image:
+			radial-gradient(at 0% 0%, rgba(var(--color-secondary-500) / 0.33) 0px, transparent 50%),
+			radial-gradient(at 98% 1%, rgba(var(--color-error-500) / 0.33) 0px, transparent 50%);
+	}
+
+	/* .first-container {
 		display: flex;
 		flex-direction: column;
 		justify-content: space-between;
@@ -32,40 +40,40 @@
 		padding: 0px;
 		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 		box-sizing: border-box;
-	}
+	} */
 
-	.first-wrapper {
+	/* .first-wrapper {
 		box-shadow: 0 0 10px black;
 		margin-bottom: 20px;
 		border-radius: 8px;
 		width: 50%;
 		padding: 20px 50px 81px;
-	}
+	} */
 
-	.first-title-wrapper {
+	/* .first-title-wrapper {
 		margin-top: 0px;
 		display: flex;
 		flex-direction: row;
 		font-size: 30px;
-	}
+	} */
 
-	.first-title-wrapper img {
+	/* .first-title-wrapper img {
 		object-fit: cover;
 		width: 25%;
 		height: 0%;
 		margin-left: auto;
-	}
+	} */
 
-	.first-title {
+	/* .first-title {
 		color: #333;
-	}
+	} */
 
-	.first-description-wrapper {
+	/* .first-description-wrapper {
 		margin-top: 0px;
 		line-height: 1.6;
-	}
+	} */
 
-	.first-button- {
+	/* .first-button- {
 		margin-top: 40px;
 		display: flex;
 		justify-content: space-between;
@@ -105,5 +113,5 @@
 		background-color: #c82333;
 		color: white;
 		transition: all ease 0.5s;
-	}
+	} */
 </style>
