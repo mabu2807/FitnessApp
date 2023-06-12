dd0ffc673d11262bcff4fc3131bae0791aae6af8
StabilesMuli <justin.schwab@t-online.de>
2023-06-12 18:42:30 +0200

    overview getStarted

--- src/components/Overview.svelte
+++ src/components/Overview.svelte
@@ -4,60 +4,16 @@
 </script>
 
 <section class="h-80">
-
-
-	<div class="my-12 overview">
+<div class="justify-center flex sm:my-12 my-8">
+	<div class="grid md:grid-cols-3 grid-cols-2 gap-y-8 md:gap-x-14 gap-x-8">
 		{#each $getStartedData as page, i}
-			<div class="overview-wrapper">
-				<div class="overview-link">{page.title}</div>
-				<p>{page.value}</p>
+			<div>
+				<p class="sm:text-base text-sm font-bold">{page.title}</p>
+				<p class="sm:text-base text-sm text-surface-400 dark:text-primary-500">{page.value}</p>
 			</div>
 		{/each}
 	</div>
+</div>
 </section>
 
-<style>
-
-
-
-	.overview {
-		/* position: fixed;
-		bottom: 0;
-		left: 0; */
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-		width: 100%;
-		height: 15%;
-		font-size: 0.9rem;
-		background-color: #393e46;
-	}
-
-	.overview-wrapper {
-		display: flex;
-		flex-direction: column;
-		margin-right: 40px;
-		width: 150px;
-		height: 50px;
-		max-width: 150px;
-	}
-
-	.overview-wrapper p {
-		font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
-			Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-		color: white;
-		font-weight: 700;
-	}
-	.overview-link {
-		color: white;
-		text-decoration: none;
-		transition: color 0.3s;
-		color: rgb(193, 193, 193);
-		font-size: 17px;
-		font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
-			Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-		max-width: 140px;
-		font-weight: 700;
-	}
-</style>
+  
