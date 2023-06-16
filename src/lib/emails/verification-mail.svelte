2b7731048a6a7f805c8bb3fecd46adb832f1a1eb
StabilesMuli <justin.schwab@t-online.de>
2023-06-16 09:20:49 +0200

    nodemailer-mailgun

---
+++ src/lib/emails/verification-mail.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { Container, Head, Html, Img, Preview } from 'svelte-email';
+
+	export let name: string;
+</script>
+
+<Html lang="de">
+	<Head />
+	<Preview preview="Willkommen bei FitnessHub!" />
+	<Container>
+		<Img src="src/assets/Logo.jpg" alt="Fitnesshub Logo" width="200" height="200"/>
+		<p>Willkommen bei FitnessHub {name}!</p>
+		<p>Bitte bestätige deine E-Mail indem du auf den folgenden Knopf drückst:</p>
+		<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+			<button>E-Mail bestätigen</button>
+		</a>
+		<p>	<br>
+			Mit sportlichen Grüßen<br>
+			<br>
+			Justin-Thomas vom Fitnesshub Team
+		</p>
+	</Container>
+	<footer class="footer">
+		<div class="footer-content">
+			<p class="footer-text">&copy; 2023 Fitness Tracker. All rights reserved.</p>
+			<a class="linkFooter" href="https://www.youtube.com/@RoswitaRuhl"
+				><i class="fa-brands fa-youtube fa-2x" /></a
+			>
+			<a class="linkFooter" href="https://www.instagram.com/ksvlangen/"
+				><i class="fa-brands fa-instagram fa-2x" /></a
+			>
+			<a class="linkFooter" href="https://twitter.com/mibi61?s=21&t=O8Ege5KSxyYHHBAtUfSaJQ"
+				><i class="fa-brands fa-square-twitter fa-2x" /></a
+			>
+			<a class="linkFooter" href="https://www.linkedin.com/in/luca-chmielarski?original_referer="
+				><i class="fa-brands fa-linkedin fa-2x" /></a
+			>
+		</div>
+	</footer>
+</Html>
--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,6 +1,67 @@
+// import prisma from '$lib/prisma';
+// import { fail, redirect } from '@sveltejs/kit';
+// import type { Actions } from './$types';
+
+// const validateEmail = (email: string) => {
+// 	return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+// };
+
+// export const actions = {
+// 	default: async ({ request }) => {
+// 		const data = await request.formData();
+
+// 		const email = data.get('email');
+// 		const username = data.get('username');
+// 		const password = data.get('password');
+// 		const passwordConfirm = data.get('passwordConfirm');
+
+// 		if (!email || !username || !password || !passwordConfirm) {
+// 			return fail(400, { email, username, password, passwordConfirm, missing: true });
+// 		}
+
+// 		if (
+// 			typeof email != 'string' ||
+// 			typeof username != 'string' ||
+// 			typeof password != 'string' ||
+// 			typeof passwordConfirm != 'string'
+// 		) {
+// 			return fail(400, { incorrect: true });
+// 		}
+
+// 		if (!validateEmail) {
+// 			return fail(422, { email });
+// 		}
+
+// 		if (password !== passwordConfirm) {
+// 			return fail(401, { email, username, password });
+// 		}
+
+// 		//Check if email already exists
+// 		const user = await prisma.user.findUnique({
+// 			where: { email: email }
+// 		});
+// 		if (user) {
+// 			return fail(409, { email });
+// 		}
+
+// 		await prisma.user.create({
+// 			data: {
+// 				email: email,
+// 				username: username,
+// 				password: password
+// 			}
+// 		});
+
+// 		throw redirect(303, `/`);
+// 	}
+// } satisfies Actions;
+
+
+
 import prisma from '$lib/prisma';
 import { fail, redirect } from '@sveltejs/kit';
-import type { Actions } from './$types';
+import type { Actions } from '../register/$types';
+import { sendEmail } from '$lib/emails/SendVerificationMail';
 
 const validateEmail = (email: string) => {
 	return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
@@ -8,12 +69,12 @@ const validateEmail = (email: string) => {
 
 export const actions = {
 	default: async ({ request }) => {
-		const data = await request.formData();
+		const data = Object.fromEntries(await request.formData());
 
-		const email = data.get('email');
-		const username = data.get('username');
-		const password = data.get('password');
-		const passwordConfirm = data.get('passwordConfirm');
+		const email = data.email;
+		const username = data.username;
+		const password = data.password;
+		const passwordConfirm = data.passwordConfirm;
 
 		if (!email || !username || !password || !passwordConfirm) {
 			return fail(400, { email, username, password, passwordConfirm, missing: true });
@@ -36,6 +97,8 @@ export const actions = {
 			return fail(401, { email, username, password });
 		}
 
+		// await sendEmail(email, username);
+
 		//Check if email already exists
 		const user = await prisma.user.findUnique({
 			where: { email: email }
@@ -51,6 +114,8 @@ export const actions = {
 				password: password
 			}
 		});
+
+		// await sendEmail(email, username);
 
 		throw redirect(303, `/`);
 	}
