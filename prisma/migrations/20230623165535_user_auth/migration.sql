792a0c4b29a430811e4d70b2d67d1766a80abe7b
Adrian Hagen <adrian.hagen@sap.com>
2023-07-04 18:54:33 +0200

    added pictures to user

--- prisma/migrations/20230623165535_user_auth/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `auth_method` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `user` ADD COLUMN `auth_method` VARCHAR(191) NOT NULL,
-    MODIFY `password` VARCHAR(191) NULL;
