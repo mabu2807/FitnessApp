fb267e5d64d3e17d8dd5c49f31639ab8c34a829e
Marcel Bulling <77400133+mabu2807@users.noreply.github.com>
2023-07-11 20:42:49 +0200

    delete cookies (#114)

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,23 +1,8 @@
 import type { LayoutServerLoad } from './$types';
-import prisma from '$lib/prisma';
-import { redirect } from '@sveltejs/kit';
 
-export const load: LayoutServerLoad = async ({ locals, cookies }) => {
-	const session = await locals.getSession();
-	let user;
-	if (session?.user) {
-		try {
-			 user = await prisma.user.findUniqueOrThrow({
-				where: {
-					email: session.user.email ?? ''
-				}
-			});
-		} catch (error) {
-			throw redirect(303, '/login');
-		}
-		cookies.set('user_id', user.id.toString())
-	}
+export const load: LayoutServerLoad = async (event) => {
+	
 	return {
-		session: await locals.getSession()
+		session: await event.locals.getSession()
 	};
 };
