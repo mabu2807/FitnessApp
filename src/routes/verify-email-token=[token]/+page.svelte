0031d3d9b996ce25aa1d3bb5111f5fbd24a4551a
StabilesMuli <justin.schwab@t-online.de>
2023-07-14 11:45:04 +0200

    verify email

--- src/routes/verify-email-token=[token]/+page.svelte
+++ src/routes/verify-email-token=[token]/+page.svelte
@@ -1,9 +1,42 @@
-<div>
-	<head>
-		<title>E-Mail verifiziert</title>
-	</head>
-	<body>
-		<h1>Danke, dass du deine E-Mail verifiziert hast.</h1>
-		<p>Klicke <a href="/">hier</a>, um zur Startseite zu gelangen.</p>
-	</body>
+<div class="flex flex-col dark:bg-surface-800">
+	<section class="flex items-center justify-center mt-32 mb-40">
+		<!-- class first-wrapper -->
+		<div class="card top-gradient lg:w-2/3 xl:w-1/2 w-3/4 m-10 py-10 sm:px-12 px-8 shadow-xl">
+			<div class="text-center mb-8">
+				<h2 class="h2 font-medium">Willkommen bei FitnessHub!</h2>
+				<!-- <img src="Logo.jpg" alt="logo" /> -->
+			</div>
+			<div class="flex flex-col justify-center items-center">
+
+				<p class="text-sm mb-16" >
+					Du hast <b style="font-wieght: bold"> erfolgreich </b> deine E-Mail <b style="font-wieght: bold"> bestätigt</b>! <i class="fa-solid fa-check" style="color: green;"></i><i style="color: green;" class="fa-solid fa-check"></i><i style="color: green;" class="fa-solid fa-check"></i>  <br> <br> Melde dich nun an und werde stärker als Markus Masse!
+					<br>
+					Zusammen erreichen wir deine Ziele!
+				</p>
+				
+				
+				<div class="flex justify-between">
+					<a href="/login"
+					><span
+					class="btn variant-filled px-3 py-2 transition duration-400 hover:bg-tertiary-500 dark:hover:bg-primary-500"
+					>Melde dich an!</span
+					></a
+					>
+				</div>
+				
+			</div>
+		</div>
+	</section>
 </div>
+<div class="flex-grow" />
+
+<style>
+	.top-gradient {
+		background-image: radial-gradient(
+				at 0% 0%,
+				rgba(var(--color-secondary-500) / 0.33) 0px,
+				transparent 50%
+			),
+			radial-gradient(at 98% 1%, rgba(var(--color-secondary-200) / 0.33) 0px, transparent 50%);
+	}
+</style>
